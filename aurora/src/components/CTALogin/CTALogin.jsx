566077581e22b4a857a5565d5a3b6184e272b752
GabrielCostaMarques <gabrielcostamarques2@gmail.com>
2024-04-16 17:27:26 -0300

    CTA block and more product block with otimization code fakeapi

---
+++ aurora/src/components/CTALogin/CTALogin.jsx
@@ -0,0 +1,16 @@
+import styles from "./CTALogin.module.css"
+
+export default function CTALogin() {
+    return (
+        <section>
+            <div className={styles.ImgParallax}>
+                <div>
+                    <h3>FAÇA SUA INSCRIÇÃO PARA RECEBER OFERTAS</h3></div>
+                <div>
+                    <button>CLIQUE AQUI</button>
+                </div>
+            </div>
+
+        </section>
+    )
+}
--- aurora/src/components/ProductsTable/ProductsTable.jsx
+++ aurora/src/components/ProductsTable/ProductsTable.jsx
@@ -1,47 +1,35 @@
 import styles from './ProductsTable.module.css'
-
 import images from "../../assets/imges-export"
 
 export default function ProductsTable() {
 
+    const products=[
 
+        {name:"Bolsa Preta",price:99.99,image:images.image4},
+        {name:"Bolsa Preta",price:99.99,image:images.image4},
+        {name:"Bolsa Preta",price:99.99,image:images.image4},
+        {name:"Bolsa Preta",price:99.99,image:images.image4},
+    ]
     return (
 
         <section className={styles.BodyProducts}>
-
             <h2>CONHEÇA MAIS DE NOSSOS PRODUTOS</h2>
-            
+            <br></br>
+            <br></br>
+            <br></br>
             <div className={styles.TableMain}>
-                <div className={styles.ItemProduct}>
-                    <div >
-                        <img src={images.image4}></img>
-                        <div className={styles.BlockNamePrice}>
-                            <h5>Bolsa preta</h5>
-                            <h4>R$99,99</h4>
+                {products.map((product, index) => (
+                    <div key={index} className={styles.ItemProduct}>
+                        <div>
+                            <img src={product.image}></img>
+                            <div className={styles.BlockNamePrice}>
+                                <h5>{product.name}</h5>
+                                <h4>R$ {product.price}</h4>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className={styles.ItemProduct}>
-                    <div >
-                        <img src={images.image4}></img>
-                        <div className={styles.BlockNamePrice}>
-                            <h5>Bolsa preta</h5>
-                            <h4>R$99,99</h4>
-                        </div>
-                    </div>
-                </div>
-                <div className={styles.ItemProduct}>
-                    <div >
-                        <img src={images.image4}></img>
-                        <div className={styles.BlockNamePrice}>
-                            <h5>Bolsa preta</h5>
-                            <h4>R$99,99</h4>
-                        </div>
-                    </div>
-                </div>
+                ))}
             </div>
-
         </section>
-
     )
 }
