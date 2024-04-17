7d75b4757c79fdcaaaf2dd77fb190b7da9e368fe
Luizynhoo <lhenrique1804@gmail.com>
2024-04-17 16:29:13 -0300

    Menu

--- aurora/src/components/Header/Header.jsx
+++ aurora/src/components/Header/Header.jsx
@@ -1,21 +1,38 @@
 import styles from './Header.module.css';
 import Logo from '../../assets/logo-rgb.png';
+//Importando a funcionalidade e os botões utilizando o React-icons
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { useRef } from 'react';
 
 
 export default function Header() {
+
+    const navRef = useRef();
+
+    const showNavbar = () => {
+        navRef.current.classList.toggle("responsive_nav");
+    }
+
     return (
         <header>
-            
+
             <div className={styles.headerContent}>
-            <div className={styles.boxLogo}>
-                <img className={styles.logo} src={Logo} alt="Logo Aurora" />
-            </div>
-                <nav>
+                <div className={styles.boxLogo}>
+                    <img className={styles.logo} src={Logo} alt="Logo Aurora" />
+                </div>
+                <nav ref={navRef}>
                     <a href="#overview">OVERVIEW</a>
                     <a href="#projetos">PROMOÇÕES</a>
                     <a href="#projetos">LUXO</a>
                     <a href="#projetos">CUSTO BENEFÍCIO</a>
+                    {/* Botão hamburguer */}
+                    <button id="btn" className="nav-btn nav-fecha-btn" onClick={showNavbar}>
+                        <FaTimes />
+                    </button>
                 </nav>
+                <button id="btn" className="nav-btn" onClick={showNavbar}>
+                    <FaBars />
+                </button>
             </div>
 
         </header>
