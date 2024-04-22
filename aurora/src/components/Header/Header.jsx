b0a673a1cd97e6752d47ff0b42dc032f80644404
GabrielCostaMarques <gabrielcostamarques2@gmail.com>
2024-04-22 16:39:28 -0300

    liberacao de commit

--- aurora/src/components/Header/Header.jsx
+++ aurora/src/components/Header/Header.jsx
@@ -1,18 +1,12 @@
 import styles from './Header.module.css';
 import Logo from '../../assets/logo-rgb.png';
-//Importando a funcionalidade e os botões utilizando o React-icons
-import { FaBars, FaTimes } from 'react-icons/fa';
-import { useRef } from 'react';
+
 
 
 export default function Header() {
-
-    const navRef = useRef();
-
-    const showNavbar = () => {
-        navRef.current.classList.toggle("responsive_nav");
-    }
-
+ 
+    console.log(document.getElementById("#overview"));
+   
     return (
         <header>
 
@@ -20,21 +14,28 @@ export default function Header() {
                 <div className={styles.boxLogo}>
                     <img className={styles.logo} src={Logo} alt="Logo Aurora" />
                 </div>
-                <nav ref={navRef}>
-                    <a href="#overview">OVERVIEW</a>
+                <nav>
+                    <a id="#overview">OVERVIEW</a>
                     <a href="#projetos">PROMOÇÕES</a>
                     <a href="#projetos">LUXO</a>
                     <a href="#projetos">CUSTO BENEFÍCIO</a>
-                    {/* Botão hamburguer */}
-                    <button id="btn" className="nav-btn nav-fecha-btn" onClick={showNavbar}>
-                        <FaTimes />
-                    </button>
                 </nav>
-                <button id="btn" className="nav-btn" onClick={showNavbar}>
-                    <FaBars />
-                </button>
+            </div>
+            <div className={styles.megaMenu}>
+                <div>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias, reprehenderit.
+                </div>
+                <div>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias, reprehenderit.
+                </div>
+                <div>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias, reprehenderit.
+                </div>
             </div>
 
+
         </header>
+
+
     )
 }
