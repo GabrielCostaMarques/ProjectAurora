a00ebdf564fa89261d24c1c350a14fdfc0d8e3a4
GabrielCostaMarques <gabrielcostamarques2@gmail.com>
2024-05-08 18:54:43 -0300

    teste

--- aurora/src/App.jsx
+++ aurora/src/App.jsx
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
-import Header from "./components/header";
-import './App.css';
-
-function App() {
-  return (
-    <>
-      <Header/>
-=======
 
 import './App.css'
-import images from './assets/imges-export'
 import BodyScreen1 from './components/BodyScreen1/BodyScreen1'
 import CTALogin from './components/CTALogin/CTALogin'
 import Header from './components/Header/Header'
@@ -17,15 +7,14 @@ import ProductsTable from './components/ProductsTable/ProductsTable'
 
 function App() {
 
+
   return (
     <>
       <Header/>
       <BodyScreen1/>
-      <ProductsTable name="Bolsa Azul" price={300} image={images.image4} oferta={false}/>
-
+      <ProductsTable/>
       <CTALogin/>
-      <ProductsTable name="Bolsa Preta" price={30} image={images.image4} oferta={true}/>
->>>>>>> 00a3f28e8a20a1bbd4900be1c8d669367578e609
+      <ProductsTable/>
     </>
   )
 }
