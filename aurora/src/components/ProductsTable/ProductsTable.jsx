d0030114804cf5540b27634d1a05b0cb15ad72fc
GabrielCostaMarques <gabrielcostamarques2@gmail.com>
2024-05-28 22:41:51 -0300

    skeleton loader e error

--- aurora/src/components/ProductsTable/ProductsTable.jsx
+++ aurora/src/components/ProductsTable/ProductsTable.jsx
@@ -1,37 +1,51 @@
-import styles from './ProductsTable.module.css'
+import styles from './ProductsTable.module.css';
 import { useFetchItems } from '../../hooks/useFetchQuery';
 
-const URL="http://localhost:3000/products"
+const URL = "http://localhost:3000/products";
 
 export default function ProductsTable() {
-
-const {data, isLoading}=useFetchItems(URL)
-
-if(isLoading)return<p>Carregando...</p>
-
+    const { data, isLoading, isError } = useFetchItems(URL);
+    
+    
+    
 
     return (
-
         <section className={styles.BodyProducts}>
             <h2>CONHEÇA MAIS DE NOSSOS PRODUTOS</h2>
-            <br></br>
-            <br></br>
-            <br></br>
+            <br />
+            <br />
+            <br />
             <div className={styles.TableMain}>
-                {data.map((product, index) => {
-                    return (
+                {isError?(
+                    <h2>Algo de errado aconteceu</h2>
+                ):
+                
+                
+                isLoading ? (
+                    Array(3).fill().map((_, index) => (
+                        <div key={index} className={`${styles.ItemProduct}`}>
+                            <div className={styles.SkeletonImage}></div>
+                            <div className={styles.BlockNamePrice}>
+                                <div className={styles.SkeletonText}></div>
+                                {/* <div className={`skeleton skeleton-text short`}></div> */}
+                            </div>
+                        </div>
+                    ))
+                ) : (
+                    data.map((product, index) => (
                         <div key={index} className={styles.ItemProduct}>
-                            <img src={product.image}></img>
+                            <img src={product.image} alt={product.name}></img>
                             <div className={styles.BlockNamePrice}>
                                 <h5>{product.name}</h5>
                                 <h4>R$ {product.price}</h4>
                             </div>
                             {product.oferta && <p>Oferta Imperdível</p>}
                         </div>
-                    );
-                })}
-
+                    ))
+                )
+                
+                }
             </div>
         </section>
-    )
+    );
 }
