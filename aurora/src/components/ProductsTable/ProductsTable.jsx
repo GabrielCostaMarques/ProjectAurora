4fe2b50d9e4b0a581c761f196997b06a41719afa
GabrielCostaMarques <gabrielcostamarques2@gmail.com>
2024-10-22 16:02:34 -0300

    css

--- aurora/src/components/ProductsTable/ProductsTable.jsx
+++ aurora/src/components/ProductsTable/ProductsTable.jsx
@@ -2,7 +2,7 @@ import styles from './ProductsTable.module.css';
 import { useFetchItems } from '../../hooks/useFetchQuery';
 import IndividualProduct from '../IndividualProduct';
 
-const URL = "https://fakestoreapi.com/products?limit=4";
+const URL = "https://fakestoreapi.com/products?limit=20";
 export default function ProductsTable() {
     const { getRequest } = useFetchItems('products', URL);
 
